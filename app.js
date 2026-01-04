e25a531832cf563522920f14945a93be92d72e34
Imama Ansari <ansariimama@yahoo.com>
2026-01-04 10:21:59 +0500

    hackathon start

---
+++ app.js
@@ -0,0 +1,6 @@
+// =============================================
+  // SUPABASE CLIENT INITIALIZATION
+  // =============================================
+  const supabaseUrl = "https://qmujsllexgevhzpkuygo.supabase.co";
+  const supabaseKey = "sb_publishable_4HLArwo3ZhIorJ15ag0K3Q_o0do1Vk2";
+  const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
